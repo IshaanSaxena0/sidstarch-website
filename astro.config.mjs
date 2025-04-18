dc5ce250e9b31f35f72ab02280e6e5e680117213
Ishaan Saxena <Saxenaishaan1@gmail.com>
2025-04-18 16:57:31 +0530

    ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,15 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 import mdx from "@astrojs/mdx";
+//import github from '@astrojs/github-pages';
 
 import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://IshaanSaxena0.github.io/sidstarch-website",
-	base: "/sidstarch-website/",
+	//site: "https://IshaanSaxena0.github.io/sidstarch-website",
+	//adapter: github(),
+	base: "/",
 	// trailingSlash: 'always',
 	integrations: [
 		tailwind(),
